a9b6018c1f7181a19ce2aefadb01e1be12f254b3
Feizza <feizza01@gmail.com>
2021-12-06 17:27:51 -0500

    added book model

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,17 @@ class Post(models.Model):
     #This method returns string with Post Title hence the __str__
     def __str__(self):
         return self.title
+
+class Book(models.Model):
+    author = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    summary = models.TextField
+    genre = models.CharField(max_length=200)
+    publish_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.publish_date = timezone.now()
+        self.save()
+        
+    def __str__(self):
+        return self.title
